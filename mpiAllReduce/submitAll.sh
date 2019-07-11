cb38d3ec4554f318f2f85607b61c18a636c4823f
Cameron Smith <smithc11@rpi.edu>
2019-07-11 12:18:14 -0400

    request min tasks

--- mpiAllReduce/submitAll.sh
+++ mpiAllReduce/submitAll.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-[ $# -ne 1 ] && echo "Usage: $0 <numabinding=[on|off]>" && exit 1
+[ $# -ne 2 ] && echo "Usage: $0 <numabinding=[on|off]> <coreRequest=[on|off]>" && exit 1
 binding=$1
+coreReq=$2
 for ppn in 12 24 48; do
   for nodes in 2 4 8 12; do
     p=$((ppn*nodes))
     echo $ppn $nodes $((p))
     set -x
-    ./submitOpenMPI.sh $p $ppn $binding
+    ./submitOpenMPI.sh $p $ppn $binding $coreReq
     set +x
   done
 done
